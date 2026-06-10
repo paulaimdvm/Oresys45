ec9cdf3ee900cecd56e3fa59b7fd3b38a3ee01ac
Paul-Aimery PREVERAUD DE VAUMAS <134688121+paulaimdvm@users.noreply.github.com>
2026-06-10 16:51:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from supabase import create_client
 
 # 🔑 CONFIG SUPABASE
 
-url = st.secrets["SUPABASE_URL"]
-key = st.secrets["SUPABASE_KEY"]
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
